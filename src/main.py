2b8dadddd4bee1d66658e8e13e02211a0acb6746
Tuna <117102448+jtran2003@users.noreply.github.com>
2024-02-24 18:54:45 -0500

    added statsbox

--- src/main.py
+++ src/main.py
@@ -4,7 +4,16 @@ from pygame.locals import *
 
 pygame.init()
 
-canvas = pygame.display.set_mode((800,500))
+# Create Frame
+canvas = pygame.display.set_mode((1000,500))
+canvas.fill('chartreuse4')
+
+# Creating Path
+pathway = pygame.Surface((200,500))
+pathway.fill('khaki3')
+
+statBox = pygame.Surface((300, 500))
+statBox.fill('blue')
 
 pygame.display.set_caption("Growth Game")
 gameOn = True
@@ -13,5 +22,8 @@ while gameOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOn = False
+    
+    canvas.blit(pathway, (550,0 ))
+    canvas.blit(statBox, (0, 0))
     
     pygame.display.update()
---
+++ src/user.py
@@ -0,0 +1,7 @@
+class user(self):
+    self.name = ""
+    self.age = 0
+    self.grade = 6
+    self.gender = ""
+    
+    pass
