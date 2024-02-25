a9aebdb442609db7b64678d306fa078edc697e98
Tejae <tejae11@my.yorku.ca>
2024-02-24 19:46:53 -0500

    Draft Player Position

--- src/main.py
+++ src/main.py
@@ -3,15 +3,20 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+clock = pygame.time.Clock()
 
 # Create Frame
 canvas = pygame.display.set_mode((1000,500))
 canvas.fill('chartreuse4')
 
-
 # Creating Path
 pathway = pygame.image.load('C:/Users/jzham/PROJECTS/GrowthGame/src/Assets/path.png')
 pathway = pygame.transform.scale(pathway, (150,500))
+
+# Creating Player
+player = pygame.Surface((25,25))
+player.fill('Red')
+playerYPos = 50
 
 
 pygame.display.set_caption("Growth Game")
@@ -22,7 +27,11 @@ while gameOn:
         if event.type == pygame.QUIT:
             gameOn = False
     
-    # canvas.blit(grass, (0,0))
     canvas.blit(pathway, (575,0))
+    canvas.blit(player, (640,playerYPos))
+    playerYPos += 2
+    canvas.blit(player, (640,playerYPos))
+
     
     pygame.display.update()
+    clock.tick(60)
