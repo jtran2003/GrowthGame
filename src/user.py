0f0457ae9d75c6c35da6d9f27ce1f87b0995ed1a
EdisonnTran <tran.edison52@gmail.com>
2024-02-24 20:19:30 -0500

    Implement scheduling functionality

--- src/user.py
+++ src/user.py
@@ -1,4 +1,11 @@
 import skills
+import cohere
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+COHERE_KEY = os.getenv("COHERE_KEY")
+co = cohere.Client(COHERE_KEY)
 
 class User:
     
@@ -13,13 +20,33 @@ class User:
         self.age = 11
         self.gender = ''
         self.schedule = []
+        self.action_history = []
         self.skills = [academics, athletics, creativity]
 
-    def addToSchedule():
-        pass
+    def set_schedule(self, sched):
+        self.schedule = sched
+        
 
-    def updateSkills():
-        pass
+    def update_skills(self):
+        self.action_history.append(self.schedule)
+        update = [0] * 3
+        DOCS = ["academics", "athletics", "creativity"]
+
+        for skill in self.schedule:
+            res = co.rerank(query=skill, documents=DOCS, model="rerank-english-v2.0", top_n = 1)[0]
+            update[res.index] += 1
+        
+        for i in range(len(update)):
+            self.skills[i].increment_skill(update[i])
+
+    def get_skills(self):
+        return {skill.get_name(): skill.get_level() for skill in self.skills}
+
+
+
 
 if __name__ == "__main__":
     user = User("test", "F")
+    user.set_schedule(["math", "math", "gym","history","music"])
+    user.update_skills()
+    print(user.get_skills())
